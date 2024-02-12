7564c8385fbb923fd55c2686f42df395dfb3ddea
Adams009 <olaadam003@gmail.com>
2024-02-11 19:10:29 -0800

    no message

--- models/base_model.py
+++ models/base_model.py
@@ -2,8 +2,25 @@
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel:
+    """ The BaseModel class is a base class.
+
+    Attributes:
+        - id (str): unique id genetator
+        - created_at (datetime): the timestamp for when object is created
+        - updated_at (datetime): the timestamp for when object is updated
+
+     """
     def __init__(self, *args, **kwargs):
+        """ Initializing  instances for the cclass named BaseModel
+
+        Parameters:
+         - args: this was not used
+         - kwargs (dict): Keyword arguments of the object's attributes.
+            If "__class__" is present in kwargs, it is ignored.
+            If "created_at" or "updated_at" is present, it will be on ISO.
+        """
 
         if kwargs is not None:
             for key, value in kwargs.items():
@@ -19,13 +36,27 @@ class BaseModel:
             self.updated_at = datetime.now()
 
     def __str__(self):
+        """ Returns a string representation of the BaseModel object.
+
+        Returns:
+        - str:  A formatted string
+        """
+
         the_class_name = self.__class__.__name__
         return f"[{the_class_name}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """ Updates the updated_at """
+
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """ Converts the BaseModel object to a dictionary.
+
+        Returns:
+        - dict: A dictionary
+        """
+
         iso_of_created = self.created_at.isoformat()
         iso_of_updated = self.updated_at.isoformat()
         the_class = self.__class__.__name__
@@ -38,6 +69,3 @@ class BaseModel:
         my_dict.update(self.__dict__)
 
         return my_dict
-
-if __name__ == "__main__":
-    base1 = BaseModel()
